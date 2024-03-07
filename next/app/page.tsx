02813c9bbcfc18c6ab277e610bf27982caa9dead
mrZar <mrzar1380@gmail.com>
2024-03-07 21:55:44 +0330

    inital

---
+++ next/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h5 className="font1-700">fsf </h5>
+    </main>
+  );
+}
---
+++ next/app/providers/StoreProvider.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import store from '@store/store';
+interface Props {
+  children: ReactNode;
+}
+
+export default function StoreProvider({ children }: Props) {
+  return <Provider store={store}>{children}</Provider>;
+}
